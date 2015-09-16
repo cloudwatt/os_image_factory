08ba052af5d4842eebb6ba103c9674940691b64c
A. Maury <aurelien.maury+localgit@wescale.fr>
2015-09-16 11:41:30 +0200

    wrong relative path

--- images/bundle-trusty-lamp/build.sh
+++ images/bundle-trusty-lamp/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
-SELF_PATH=`dirname "$0"`
 
-ansible-playbook ../bundle-build.playbook.yml -e "bundle_label=bundle-trusty-lamp bundle_path=$SELF_PATH bundle_src_img=ae3082cb-fac1-46b1-97aa-507aaa8f184f bundle_img_os=Ubuntu"
+ansible-playbook ../bundle-build.playbook.yml -e "bundle_label=bundle-trusty-lamp bundle_path=bundle-trusty-lamp bundle_src_img=ae3082cb-fac1-46b1-97aa-507aaa8f184f bundle_img_os=Ubuntu"
