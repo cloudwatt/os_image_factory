21283886ce50db2c8d46e7eb0a8eafe273cdc40a
A. Maury <aurelien.maury+localgit@wescale.fr>
2015-09-17 16:53:24 +0200

    eat, sleep, code, repeat

--- images/bundle-trusty-lamp/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
-
-ansible-playbook ../bundle-build.playbook.yml -e "bundle_label=bundle-trusty-lamp bundle_path=bundle-trusty-lamp bundle_src_img=ae3082cb-fac1-46b1-97aa-507aaa8f184f bundle_img_os=Ubuntu"
