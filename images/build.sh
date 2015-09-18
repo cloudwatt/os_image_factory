9b2c73de5da00666f9b9821a6e358c2ef5cf9fc4
A. Maury <aurelien.maury+localgit@wescale.fr>
2015-09-18 16:04:30 +0200

    cosmetic

--- images/build.sh
+++ images/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ansible-playbook $(dirname $0)/bundle-build.playbook.yml -e @$(dirname $0)/$1/build-vars.yml
+ansible-playbook $(dirname $0)/build.playbook.yml -e @$(dirname $0)/$1/build-vars.yml
