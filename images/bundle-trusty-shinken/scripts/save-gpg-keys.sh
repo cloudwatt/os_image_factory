89d5508643cc963308de9ec88724baefda7e8755
PRA <aurelien.maury+pra@wescale.fr>
2015-10-30 11:18:46 +0100

    added bundle shinken work in progress
    ajout de test dans pi
    
    création du bundle image
    
    md
    
    Ajout bundle shinken
    
    suppression bundle
    
    added buildvars AND renamed something already commited - hi Tanguy

---
+++ images/bundle-trusty-shinken/scripts/save-gpg-keys.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+STACK_NAME=$1
+SSH_KEY_PATH=$2
+
+if [ ! "$STACK_NAME" ] || [ ! "$SSH_KEY_PATH" ]; then
+    echo "STACK_NAME and SSH_KEY_PATH to connect with SSH to server are mandatory."
+    echo -e """\
+\e[32mUSAGE:\e[0m ./send-os-cred.sh STACK_NAME SSH_KEY_PATH
+
+Back up both of Duplicity's GnuPG keys (signing and encryption)
+to two files in your current directory.
+"""
+    exit 1
+fi
+
+if [ ! -r "$SSH_KEY_PATH" ]; then
+  echo "ERROR: SSH Key does not exist or cannot be read.";
+  exit 1;
+fi
+
+STACK_FLOATING_IP=` heat resource-list $STACK_NAME          \
+                 | grep "| OS::Nova::FloatingIPAssociation" \
+                 | cut -d"|" -f3                            \
+                 | awk -F"-" '{print $NF}'                  \
+                 `
+if [ -z "$STACK_FLOATING_IP" ]; then
+  echo "Stack floating-IP could not be found."
+  heat resource-list $STACK_NAME
+  exit 1
+else
+  echo -e "Stack floating-IP found: \e[32m${STACK_FLOATING_IP}\e[0m"
+fi
+
+ssh "$STACK_FLOATING_IP" -l cloud -i "$SSH_KEY_PATH" "echo \"Successfully echoed stack server.\""
+if [ "$?" != "0" ]; then
+    echo "Could not echo from stack server."
+    exit 1
+fi
+
+source <(ssh "$STACK_FLOATING_IP" -l cloud -i "$SSH_KEY_PATH" "sudo cat /etc/duplicity/dup_vars.sh")
+echo -e "Saving backup signing key to file \e[32m./signing.asc\e[0m"
+ssh "$STACK_FLOATING_IP" -l cloud -i "$SSH_KEY_PATH" "sudo HOME=/root gpg --export-secret-keys -a $SIGN_KEY"    > signing.asc
+echo -e "Saving backup encryption key to file \e[32m./encryption.asc\e[0m"
+ssh "$STACK_FLOATING_IP" -l cloud -i "$SSH_KEY_PATH" "sudo HOME=/root gpg --export-secret-keys -a $ENCRYPT_KEY" > encryption.asc
