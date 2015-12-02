d99956725d31e5cd7c055f778a710001ca4ba704
Pierre <pierreraymond.ane@gmail.com>
2015-12-02 15:37:35 +0100

    create file ansible/zabbix.conf.php && modified bootstrap.yml

---
+++ images/bundle-trusty-zabbix/ansible/zabbix.conf.php
@@ -0,0 +1,13 @@
+<?php
+// Zabbix GUI configuration file
+global $DB;
+
+$DB["TYPE"]       = 'MYSQL';
+$DB["SERVER"]     = 'localhost';
+$DB["PORT"]       = '0';
+$DB["DATABASE"]   = 'zabbix';
+$DB["USER"]       = 'zabbix';
+$DB["PASSWORD"]   = 'zabbix';
+?>
+
+
