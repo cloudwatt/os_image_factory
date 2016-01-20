535d179024c590b6fe7c1dff89127d38124cba5f
Pierre <pierreraymond.ane@gmail.com>
2016-01-20 10:07:17 +0100

    remove directory brouillon_file

--- images/bundle-trusty-biolinux/brouillon_biolinux/ansible.old/file.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget -qO- http://nebc.nerc.ac.uk/downloads/bl8_only/
--- images/bundle-trusty-biolinux/brouillon_biolinux/ansible.old/teslocal.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-texte1="if grep -q '^deb .*/\([a-z]*\)\+ universe$'   /etc/apt/sources.list && \
-     grep -q '^deb .*/\([a-z]*\)\+ multiverse$' /etc/apt/sources.list ; then
-      true "
-  texte2="if grep '^deb.* universe$' /etc/apt/sources.list &&  \
-     grep '^deb.*multiverse$' /etc/apt/sources.list"
-  sed  "/$texte1/$texte2/" upgrade_to_8.sh
