61002705f8fcd736f8d9ec35ede35eedcef87fe1
dalitun <lachhab.med.ali@gmail.com>
2017-07-03 19:10:14 +0200

    add bundle-jessie-factory

--- test-tools/pytesting_os_fe/cloudinit/test_cloudinit.py
+++ test-tools/pytesting_os_fe/cloudinit/test_cloudinit.py
@@ -8,7 +8,7 @@ global test_resources
 
 def test_cloudinit_package():
 
-    time.sleep(20)
+    time.sleep(40)
     ssh_stdin, ssh_stdout, ssh_stderr = test_resources['ssh_connection'].exec_command('emacs --version')
     cmd_stdout = ssh_stdout.read()
     package_installed_by_userdata_is_present = (cmd_stdout.find('GNU Emacs') != -1)
