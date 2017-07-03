514943242685e486895dd8f7f82aedf92250d685
juliend88 <mr2plaix@gmail.com>
2017-07-03 15:36:50 +0200

    remove bundle dir

--- images_fe/bundle-trusty-clamav/ansible/fileclamav.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/freshclam >> /var/log/resul_freshclam.txt
--- images_fe/bundle-trusty-cozycloud/ansible/Debconf-cozyinstall/renew_cert.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-/etc/init.d/nginx stop
-/root/letsencrypt/letsencrypt-auto certonly --standalone -d $(cat /etc/ansible/cozy-vars.yml | grep cozy_domain |cut -d"'" -f2) --standalone-supported-challenges tls-sni-01 --renew-by-default
-/etc/init.d/nginx start
