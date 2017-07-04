98fcb5502dd158233e43e7f720b9f18ed846ff6a
dalitun <lachhab.med.ali@gmail.com>
2017-07-04 19:28:48 +0200

    e

--- lib/functions.sh
+++ lib/functions.sh
@@ -198,7 +198,7 @@ curl -sS https://vpc.$OS_REGION_NAME.prod-cloud-ocb.orange-business.com/v1/$OS_P
  "dhcp_enable": "true",
  "primary_dns": "100.125.0.41",
  "secondary_dns": "100.126.0.41",
- "availability_zone":"$AZ_NAME",
+ "availability_zone":"",
  "vpc_id":"$2"
  }
 }
@@ -234,5 +234,72 @@ for floating_id in $FREE_FLOATING_IP; do
   neutron floatingip-delete $floating_id >/dev/null 2>&1
 
 done
+
+}
+
+
+function datastore {
+
+curl -sS -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'X-Auth-Token: $1' -H 'X-Language: en-us' https://rds.eu-west-0.prod-cloud-ocb.orange-business.com/rds/v1/$OS_PROJECT_ID/datastores/MySQL/versions
+
+
+}
+
+
+function get_flavor_rds {
+
+curl -sS https://rds.$OS_REGION_NAME.prod-cloud-ocb.orange-business.com/rds/v1/$OS_PROJECT_ID/flavors -H 'Content-Type: application/json' -H 'Accept: application/json' -H "X-Auth-Token: $1" -H 'X-Language: en-us' | jq '.'
+
+
+
+
+}
+function create_rds {
+
+curl -sS https://rds.$OS_REGION_NAME.prod-cloud-ocb.orange-business.com/rds/v1/$OS_PROJECT_ID/instances -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' -H "X-Auth-Token: $1" -H 'X-Language: en-us' -d @<(cat <<EOF
+{
+ "instance": {
+ "name": "rds-$BUILDMARK-ha",
+ "datastore": {
+ "type": "MySQL",
+ "version": "5.6.35"
+ },
+ "flavorRef": "c5cac226-b5d3-4169-a7e9-3c19369b4072",
+ "volume": {
+ "type": "COMMON",
+ "size": 100
+ },
+ "region": "$OS_REGION_NAME",
+ "availabilityZone": "$AZ_NAME",
+ "vpc": "$VPC_ID",
+ "nics": {
+ "subnetId": "$NET_ID"
+ },
+ "securityGroup": {
+ "id": "71c89962-34a1-45e1-b985-c481a0dfa23e"
+ },
+ "backupStrategy": {
+ "startTime": "00:00:00",
+ "keepDays": 3
+ },
+ "dbRtPd": "$2",
+ "ha": {
+ "enable": true,
+ "replicationMode": "async"
+ }
+ }
+}
+
+EOF
+) | jq '.instance.id' | sed -e 's/^"//'  -e 's/"$//'
+
+}
+
+
+function get_ip_rds {
+
+
+curl -sS -X GET -H 'Content-Type: application/json' -H 'Accept: application/json' -H "X-Auth-Token: $1" -H 'X-Language: en-us' https://rds.eu-west-0.prod-cloud-ocb.orange-business.com/rds/v1/0ea6597fb6a446c5b8d2daa6e3c39a33/instances/$2 |  jq '.instance.hostname' | sed -e 's/^"//'  -e 's/"$//'
+
 
 }
